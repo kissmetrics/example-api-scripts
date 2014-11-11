f04d3e76eeee897d64b9fb821d8845493acae260
Nate Klaiber <nate@theklaibers.com>
2014-11-11 17:05:57 -0500

    Require the Product models

--- lib/kissmetrics/client/models.rb
+++ lib/kissmetrics/client/models.rb
@@ -3,6 +3,9 @@ require File.expand_path('../models/directory', __FILE__)
 require File.expand_path('../models/account', __FILE__)
 require File.expand_path('../models/accounts', __FILE__)
 
+require File.expand_path('../models/product', __FILE__)
+require File.expand_path('../models/products', __FILE__)
+
 module KISSmetrics
   module Client
     module Models
