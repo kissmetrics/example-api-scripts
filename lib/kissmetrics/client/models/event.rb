bddae9bcc0762de61dda91dc35cc806cadbbfd99
Matthew Johnston <warmwaffles@gmail.com>
2014-11-12 11:38:13 -0600

    added documentation

--- lib/kissmetrics/client/models/event.rb
+++ lib/kissmetrics/client/models/event.rb
@@ -6,6 +6,7 @@ module KISSmetrics
       class Event
         include Comparable
 
+        # @param attributes [Hash] the event's attributes
         def initialize(attributes={})
           @attributes = attributes
         end
@@ -14,73 +15,78 @@ module KISSmetrics
           self.total <=> other.total
         end
 
+        # @return [String]
         def id
           @attributes['id']
         end
 
+        # @return [KISSmetrics::Client::Response::Links]
         def links
           KISSmetrics::Client::Response::Links.new(@attributes.fetch('links', []))
         end
 
+        # @return [String]
         def index
           @attributes['index']
         end
 
+        # @return [String]
         def product_id
           @attributes['product_id']
         end
 
+        # @return [String]
         def name
           @attributes['name']
         end
 
+        # @return [String]
         def display_name
           @attributes['display_name']
         end
 
+        # @return [String]
         def description
           @attributes['description']
         end
 
+        # @return [TrueClass] if the event is visible
         def is_visible
           @attributes['is_visible']
         end
         alias :visible? :is_visible
 
+        # @return [Fixnum]
         def total
-          @attributes.fetch('total', 0).to_i
+          @attributes.fetch('total', '0').to_i
         end
 
+        # @return [DateTime]
         def first_used_at
-          begin
-            DateTime.parse(@attributes.fetch('first_used_at', nil).to_s)
-          rescue ArgumentError
-            nil
-          end
+          DateTime.parse(@attributes.fetch('first_used_at', nil).to_s)
+        rescue ArgumentError
+          nil
         end
 
+        # @return [DateTime]
         def last_used_at
-          begin
-            DateTime.parse(@attributes.fetch('last_used_at', nil).to_s)
-          rescue ArgumentError
-            nil
-          end
+          DateTime.parse(@attributes.fetch('last_used_at', nil).to_s)
+        rescue ArgumentError
+          nil
         end
 
+        # @return [DateTime]
         def first_sent_at
-          begin
-            DateTime.parse(@attributes.fetch('first_sent_at', nil).to_s)
-          rescue ArgumentError
-            nil
-          end
+          DateTime.parse(@attributes.fetch('first_sent_at', nil).to_s)
+        rescue ArgumentError
+          nil
         end
 
+        # @return [DateTime]
         def last_sent_at
-          begin
-            DateTime.parse(@attributes.fetch('last_sent_at', nil).to_s)
-          rescue ArgumentError
-            nil
-          end
+          DateTime.parse(@attributes.fetch('last_sent_at', nil).to_s)
+        rescue ArgumentError
+          nil
         end
       end
     end
